95a646a3853963e4fc59c44163533298e149613e
Peter Taraba <peter.taraba.phd@gmail.com>
2026-03-31 09:53:42 -0700

    Delete run2.sh

--- run2.sh
+++
@@ -1,7 +0,0 @@
-unzip -o /home/pepe/Downloads/joy-browser-master.zip -d /home/pepe/Downloads/
-\cp -rf /home/pepe/Downloads/joy-browser-master/* /home/pepe/Code/joy-browser/
-
-rm /home/pepe/Downloads/joy-browser-master.zip
-rm -rf /home/pepe/Downloads/joy-browser-master/
-
-npm start
